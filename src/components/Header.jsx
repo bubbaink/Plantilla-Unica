166a01baa7f72b7844dddc2b7ad2a8cf522a69b2
bubbaink <benitez.alexander.anibal@gmail.com>
2025-08-27 17:55:13 -0300

    add menu bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
+import MenuBar from "./MenuBar";
+
+
 const Header = () => {
+    
+    const [open, setOpen] = useState(false)
+    const handleOpenMenu = () => {
+    setOpen(!open)
+    
+    }
+
     return (
-        <nav className="w-full h-2 pb-8 pt-2 border-b-2 border-gray-300 flex justify-between">
-            <div className="pl-2">
-                <img className="w-6 h-6" src="./src/assets/logo.svg" alt="" />
-            </div>
-            <div>
-                Texto
-            </div>
-            <div className="pr-2">
-                <div className="flex">
-                    <img className="w-10 h-7 pr-2" src="./src/assets/cart-shopping.svg" alt="" />
-                    <img className="w-10 h-7 pr-2" src="./src/assets/menu-bars.svg" alt="" />
+        <nav className="relative w-full border-b-2 border-gray-300">
+            <div className="flex h-2 pb-8 pt-2 justify-between">
+                <div className="pl-2">
+                    <img className="w-6 h-6" src="./src/assets/logo.svg" alt="" />
                 </div>
+                <div>
+                    Texto
+                </div>
+                <div className="pr-2">
+                    <div className="flex">
+                        <button>
+                            <img className="w-10 h-7 pr-2" src="./src/assets/cart-shopping.svg" alt="" />
+                        </button>
+                        <button onClick={()=> handleOpenMenu()}>
+                            <img className="w-10 h-7 pr-2" src="./src/assets/menu-bars.svg" alt="" />
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className={`${open ? open : "hidden"} flex justify-end pr-2`}>
+                <MenuBar/>
             </div>
         </nav>
     );
---
+++ src/components/MenuBar.jsx
@@ -0,0 +1,24 @@
+const MenuBar = () => {
+    return(
+        <div className="absolute w-80 h-56 mt-1 pt-1 border-2 border-gray-300 rounded-sm bg-white">
+            <nav className="flex flex-col h-full gap-5">
+                <span className="border-b-2 pt-3 border-gray-300">
+                    Hola, soy un menu
+                </span>
+                <span className="border-b-2 border-gray-300">
+                    Hola, soy un menu
+                </span>
+                <span className="border-b-2 border-gray-300">
+                    Hola, soy un menu
+                </span>
+                <span className="border-b-2 border-gray-300">
+                    Hola, soy un menu
+                </span>
+                <span>
+                    Hola, soy un menu
+                </span>
+            </nav>
+        </div>
+    )
+}
+export default MenuBar;
